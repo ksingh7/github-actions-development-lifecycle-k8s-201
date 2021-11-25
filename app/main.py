04e5da8ecc4e399ea9d8db77345965cdf5438fa3
ksingh7 <karan.singh731987@gmail.com>
2021-11-25 18:24:05 +0530

    fixes in app return message

--- app/main.py
+++ app/main.py
@@ -3,4 +3,4 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def read_root():
-    return {"Message": "! The most complex program human has ever created"}
+    return {"Message": "Hello World"}
