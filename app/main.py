8d4fae8387cc2d0dc8dda66acbe583f9fc20ede0
ksingh7 <karan.singh731987@gmail.com>
2022-02-14 19:49:56 +0530

    greetings

--- app/main.py
+++ app/main.py
@@ -3,4 +3,4 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def read_root():
-    return {"Message": "\o Hello i am an app, that deployed via GitHub Actions on OpenShift"}
+    return {"Message": "\o Hello i am an app, that deployed via GitHub Actions on OpenShift. We are LIVE on THE DEV SHOW :)"}
